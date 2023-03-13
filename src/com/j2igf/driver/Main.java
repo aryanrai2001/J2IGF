1f03d9afe81d337cea21b70b389d56e43a4c3c20
Aryan Rai <aryanrai3112@gmail.com>
2023-03-13 23:19:16 +0530

    Add image rendering in Window class

--- src/com/j2igf/driver/Main.java
+++ src/com/j2igf/driver/Main.java
@@ -7,5 +7,9 @@ public class Main
 	public static void main(String[] args)
 	{
 		Window window = new Window(640, 480, "Test");
+		while (true)
+		{
+			window.update();
+		}
 	}
 }
--- src/com/j2igf/framework/core/Window.java
+++ src/com/j2igf/framework/core/Window.java
@@ -1,23 +1,50 @@
 package com.j2igf.framework.core;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
 
 public class Window
 {
 	private JFrame frame;
+	private Canvas canvas;
+	private BufferedImage image;
+	private BufferStrategy strategy;
+	private Graphics graphics;
+
 	public Window(int width, int height, String title)
 	{
-		Dimension frameSize = new Dimension(width, height);
+		Dimension size = new Dimension(width, height);
 		frame = new JFrame(title);
-		frame.setResizable(false);
-		frame.setMinimumSize(frameSize);
-		frame.setPreferredSize(frameSize);
-		frame.setMaximumSize(frameSize);
-		frame.setLocationRelativeTo(null);
+		canvas = new Canvas();
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+		canvas.setMinimumSize(size);
+		canvas.setPreferredSize(size);
+		canvas.setMaximumSize(size);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(canvas, BorderLayout.CENTER);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setFocusable(true);
 		frame.requestFocusInWindow();
+
+		canvas.createBufferStrategy(2);
+		strategy = canvas.getBufferStrategy();
+		graphics = strategy.getDrawGraphics();
+	}
+
+	public void update()
+	{
+		graphics.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight(), null);
+		strategy.show();
 	}
 }
